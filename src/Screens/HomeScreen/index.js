c6e98d7ed6804fc650311eacedf779f4e9502046
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-12-31 13:27:20 +0530

    basic screens added

---
+++ src/Screens/HomeScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Styles from './styles';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+export default HomeScreen;
---
+++ src/Screens/ProductScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Styles from './styles';
+
+const ProductScreen = () => {
+  return (
+    <View>
+      <Text>ProductScreen</Text>
+    </View>
+  );
+};
+export default ProductScreen;
